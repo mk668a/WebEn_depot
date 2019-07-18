f4ff598d337d8908759e25ccdc85482cffda96d7
mk668a <msftoaa@gmail.com>
2019-07-18 11:08:24 +0900

    create adress validation

--- depot/app/models/airticle.rb
+++ depot/app/models/airticle.rb
@@ -1,5 +1,8 @@
 class Airticle < ApplicationRecord
   validates :name, :affiliation, :contactaddress, :phonenumber, :title, :abstract, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :contactaddress, format: { with: VALID_EMAIL_REGEX }
 #
   validates :name, uniqueness: true
 end
