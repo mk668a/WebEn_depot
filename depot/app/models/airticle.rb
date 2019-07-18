95aeb7694645fde955f1618af0bde602a4ce049c
mk668a <msftoaa@gmail.com>
2019-07-18 11:28:14 +0900

    add depot_e

--- depot/app/models/airticle.rb
+++ depot/app/models/airticle.rb
@@ -3,6 +3,9 @@ class Airticle < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :contactaddress, format: { with: VALID_EMAIL_REGEX }
-#
+
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  validates :phonenumber, presence: true, format: { with: VALID_PHONE_REGEX }
+
   validates :name, uniqueness: true
 end
---
+++ depot_e/vendor/bundle/gems/ffi-1.11.1/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/mk/Desktop/git/WebEn_depot/depot_e/vendor/bundle/gems/ffi-1.11.1/ext/ffi_c/libffi/src/x86/ffitarget.h
