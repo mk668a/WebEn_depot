730bc2b0a698140d3ab1f80892af6b4330991715
mk668a <msftoaa@gmail.com>
2019-07-22 11:07:24 +0900

    done depot_10

--- depot_10/config/routes.rb
+++ depot_10/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root 'store#index', as: 'store'
 
-  resources :products
+  resources :airticles
   # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
 end
