e69ba10fb0f7f51eaca2152ba28584b23f86b7ea
mk668a <msftoaa@gmail.com>
2019-07-27 15:36:01 +0900

    create quantity

--- depot_e/app/controllers/line_items_controller.rb
+++ depot_e/app/controllers/line_items_controller.rb
@@ -31,8 +31,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to store_index_url }
-        format.js   { @current_item = @line_item }
+        format.html { redirect_to @line_item.cart,
+          notice: 'Line item was successfully created.' }
         format.json { render :show,
           status: :created, location: @line_item }
       else
@@ -75,6 +75,7 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id)
+      params.require(:line_item).permit(:product_id, :cart_id)
     end
+  #...
 end
